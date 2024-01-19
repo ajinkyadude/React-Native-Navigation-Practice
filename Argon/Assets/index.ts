99fbfe09f3b1ea7752cf7d5226d491a43543f3f1
ajinkyadude <ajinkyadude29@gmail.com>
2024-01-19 20:12:31 +0530

    working on profile

--- Argon/Assets/index.ts
+++ Argon/Assets/index.ts
@@ -8,6 +8,12 @@ import Nature from './Nature.png';
 import BlueLogo from './blueLogo.png';
 import Google from './google.png';
 import ProfileImage from './profileImage.png';
+import One from './1.png';
+import Two from './2.png';
+import Three from './3.png';
+import Four from './4.png';
+import Five from './5.png';
+import Six from './6.png';
 
 export {
   SplashScreen,
@@ -19,5 +25,11 @@ export {
   Nature,
   BlueLogo,
   Google,
-  ProfileImage
+  ProfileImage,
+  One,
+  Two,
+  Three,
+  Four,
+  Five,
+  Six
 };
--- Argon/Screens/Profile/Profile.tsx
+++ Argon/Screens/Profile/Profile.tsx
@@ -2,6 +2,7 @@ import {
   Image,
   ImageBackground,
   SafeAreaView,
+  ScrollView,
   Text,
   TouchableOpacity,
   View,
@@ -11,112 +12,193 @@ import Icon2 from 'react-native-vector-icons/Ionicons';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import Constant from '../../Common/Constant';
 import Styles from './ProfileStyle';
+import {One, Three, Two, Five, Four, Six} from '../../Assets';
 
 const Profile = ({navigation}: any) => {
   const backHandler = () => {
     navigation.goBack();
   };
+  const arr = [One, Two, Three, Four, Five, Six];
   return (
-    <ImageBackground
-      source={SplashScreen}
-      style={{width: '100%', height: '70%'}}>
-      <SafeAreaView>
-        <View style={Styles.headerContainer}>
-          <View style={Styles.headerFirstSection}>
-            <TouchableOpacity
-              onPress={backHandler}
-              style={Styles.backHandlerStyle}>
-              <Icon2 name={Constant.backLogo} size={30} color={'#fff'} />
-            </TouchableOpacity>
-            <Text style={Styles.headerTitleStyle}>{Constant.profileText}</Text>
+    <ScrollView>
+      <ImageBackground
+        source={SplashScreen}
+        style={{width: '100%', height: '70%'}}>
+        <SafeAreaView>
+          <View style={Styles.headerContainer}>
+            <View style={Styles.headerFirstSection}>
+              <TouchableOpacity
+                onPress={backHandler}
+                style={Styles.backHandlerStyle}>
+                <Icon2 name={Constant.backLogo} size={30} color={'#fff'} />
+              </TouchableOpacity>
+              <Text style={Styles.headerTitleStyle}>
+                {Constant.profileText}
+              </Text>
+            </View>
+            <View style={Styles.headerSecondSection}>
+              <Icon
+                name={Constant.bellIcon}
+                color={'#fff'}
+                size={21}
+                style={Styles.iconStyle}
+              />
+              <Icon
+                name={Constant.shoppingIcon}
+                color={'#fff'}
+                size={21}
+                style={Styles.iconStyle}
+              />
+            </View>
           </View>
-          <View style={Styles.headerSecondSection}>
-            <Icon
-              name={Constant.bellIcon}
-              color={'#fff'}
-              size={21}
-              style={Styles.iconStyle}
+          <View style={{alignItems: 'center'}}>
+            <Image
+              source={ProfileImage}
+              style={{
+                width: 220,
+                height: 220,
+                position: 'absolute',
+                top: 0,
+                zIndex: 1,
+              }}
             />
-            <Icon
-              name={Constant.shoppingIcon}
-              color={'#fff'}
-              size={21}
-              style={Styles.iconStyle}
-            />
-          </View>
-        </View>
-        <View style={{alignItems: 'center'}}>
-          <Image
-            source={ProfileImage}
-            style={{
-              width: 220,
-              height: 220,
-              position: 'absolute',
-              top: 0,
-              zIndex: 1,
-            }}
-          />
-          <View
-            style={{
-              backgroundColor: '#fff',
-              width: '90%',
-              height: '100%',
-              marginTop: 100,
-              alignItems: 'center',
-              borderRadius: 5,
-            }}>
             <View
               style={{
+                backgroundColor: '#fff',
+                width: '90%',
+                // height: '100%',
                 marginTop: 100,
-                flexDirection: 'row',
-                width: '60%',
-                justifyContent: 'space-around',
+                alignItems: 'center',
+                borderRadius: 5,
               }}>
-              <TouchableOpacity
+              <View
                 style={{
-                  padding: 10,
-                  backgroundColor: '#11CDEF',
-                  borderRadius: 5,
+                  marginTop: 100,
+                  flexDirection: 'row',
+                  width: '60%',
+                  justifyContent: 'space-around',
                 }}>
-                <Text style={{color: 'white', fontWeight: 'bold'}}>
-                  CONNECT
-                </Text>
-              </TouchableOpacity>
-              <TouchableOpacity
+                <TouchableOpacity
+                  style={{
+                    padding: 10,
+                    backgroundColor: '#11CDEF',
+                    borderRadius: 5,
+                  }}>
+                  <Text style={{color: 'white', fontWeight: 'bold'}}>
+                    CONNECT
+                  </Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                  style={{
+                    padding: 10,
+                    backgroundColor: '#172B4D',
+                    borderRadius: 5,
+                  }}>
+                  <Text style={{color: 'white', fontWeight: 'bold'}}>
+                    MESSAGE
+                  </Text>
+                </TouchableOpacity>
+              </View>
+
+              <View
                 style={{
-                  padding: 10,
-                  backgroundColor: '#172B4D',
-                  borderRadius: 5,
+                  flexDirection: 'row',
+                  width: '100%',
+                  // justifyContent: 'space-evenly',
+                  marginTop: 30,
+                  // backgroundColor: 'red'
                 }}>
-                <Text style={{color: 'white', fontWeight: 'bold'}}>
-                  MESSAGE
+                <View style={{alignItems: 'center', width: '33.33%'}}>
+                  <Text
+                    style={{
+                      fontWeight: 'bold',
+                      color: '#525F7F',
+                      fontSize: 18,
+                      marginBottom: 7,
+                    }}>
+                    2k
+                  </Text>
+                  <Text style={{color: '#525F7F'}}>Friends</Text>
+                </View>
+                <View style={{alignItems: 'center', width: '33.33%'}}>
+                  <Text
+                    style={{
+                      fontWeight: 'bold',
+                      color: '#525F7F',
+                      fontSize: 18,
+                      marginBottom: 7,
+                    }}>
+                    10
+                  </Text>
+                  <Text style={{color: '#525F7F'}}>Photos</Text>
+                </View>
+                <View style={{alignItems: 'center', width: '33.33%'}}>
+                  <Text
+                    style={{
+                      fontWeight: 'bold',
+                      color: '#525F7F',
+                      fontSize: 18,
+                      marginBottom: 7,
+                    }}>
+                    89
+                  </Text>
+                  <Text style={{color: '#525F7F'}}>Comments</Text>
+                </View>
+              </View>
+
+              <View style={{marginTop: 40, alignItems: 'center'}}>
+                <Text style={{color: '#32325D', fontSize: 28}}>
+                  Jessica Jones, 27
                 </Text>
-              </TouchableOpacity>
-            </View>
-            <View
-              style={{
-                flexDirection: 'row',
-                width: '100%',
-                justifyContent: 'space-evenly',
-                marginTop: 30,
-              }}>
-              <View style={{alignItems: 'center'}}>
-                <Text>2k</Text>
-                <Text>Friends</Text>
+                <Text style={{color: '#32325D', fontSize: 16}}>
+                  San Francisco, USA
+                </Text>
               </View>
-              <View style={{alignItems: 'center'}}>
-                <Text>10</Text>
-                <Text>Photos</Text>
+              <View
+                style={{
+                  borderColor: '#E9ECEF',
+                  borderWidth: 1,
+                  width: '80%',
+                  marginTop: 50,
+                }}
+              />
+              <View style={{alignItems: 'center', marginTop: 20}}>
+                <Text style={{color: '#525F7F', fontSize: 16}}>
+                  An artist of considerable range, jessica
+                </Text>
+                <Text style={{color: '#525F7F', fontSize: 16}}>
+                  name taken by Melbourne ...
+                </Text>
               </View>
-              <View style={{alignItems: 'center'}}>
-                <Text>89</Text>
-                <Text>Comments</Text>
+              <View style={{marginTop: 20}}>
+                <Text style={{color: '#233DD2', fontSize: 18}}>Show more</Text>
               </View>
+              <View style={{width: '90%', marginTop: 20}}>
+                <Text
+                  style={{color: '#525F7F', fontWeight: 'bold', fontSize: 16}}>
+                  Album
+                </Text>
+                <Text
+                  style={{
+                    color: '#5E72E4',
+                    alignSelf: 'flex-end',
+                    marginTop: 20,
+                  }}>
+                  View all
+                </Text>
+              </View>
+
+              <View style={{flexDirection: 'row',flexWrap: 'wrap', justifyContent: 'center', marginBottom: 50}}>{arr && arr.map((item, index): any => {
+                return(
+                    <Image source={item} style={{width: 100, height: 100, margin: 5, borderRadius: 4}} />
+                )
+                
+              })}</View>
             </View>
           </View>
-        </View>
-      </SafeAreaView>
-    </ImageBackground>
+        </SafeAreaView>
+      </ImageBackground>
+    </ScrollView>
   );
 };
 
